dec9924bbb07bd0f3628e5d618d786ffe87736eb
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-11-17 00:20:23 +0000

    Fixed the counter function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,18 @@
-$(document).ready(function() {
-  // --- our code goes here ---
-  console.log("hello")
+$(document).ready(() => {
+  const counter = $('.counter')[0];
+  const textArea = $('textarea');
+  
+  $(textArea).on('input', function() {
+    let textCount = $(this).val();
+    $(counter).text(140 - textCount.length);
+
+    if (textCount.length > 140) {
+      $(counter).addClass('redLimit');
+    } else {
+      $(counter).removeClass('redLimit');
+    }
+
+
+  });
+ 
 });
