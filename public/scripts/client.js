c71f099d706c6e51ee71a72696312a4c4afc5106
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-11-18 22:38:16 +0000

    Removed the hard-coded tweets object

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 // Fake data taken from initial-tweets.json
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
 
 const renderTweets = function(tweets) {
   for (const tweet of tweets) {
