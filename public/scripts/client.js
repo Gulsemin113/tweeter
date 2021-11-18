bbcce869a692fcd4f89b8803b9ab275d28d970de
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-11-18 03:57:00 +0000

    Some makeup

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,7 +11,7 @@ const tweetData = {
     "handle": "@SirIsaac"
   },
   "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    "text": "If I have seen further it is by standing on the shoulders of giants"
   },
   "created_at": 1461116232227
 }
@@ -23,22 +23,22 @@ const renderTweets = function(tweets) {
 }
 
 const createTweetElement = function(tweet) {
-let $tweet = `
-<article >
-  <header>
-    <a><i class="fa-brands fa-canadian-maple-leaf"></i>${tweetData.user.name}</a>
-    <span class = "username">${tweetData.user.handle}</span>
-  </header>
-  <p> ${tweetData.content.text}</p>
-  <footer>
-    <span class = "time-left">${tweetData.created_at}</span>
-      <div class = "tweet-icons">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </div>
-  </footer>
-</article>`
+  let $tweet = (`
+    <article class = "tweets">
+      <header>
+        <a><i class="fa-brands fa-canadian-maple-leaf"></i>${tweet.user.name}</a>
+        <span class = "username">${tweet.user.handle}</span>
+      </header>
+      <p> ${tweet.content.text}</p>
+      <footer>
+        <span class = "time-left">${tweet.created_at}</span>
+          <div class = "tweet-icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+      </footer>
+    </article>`);
 
 return $tweet;
 }
